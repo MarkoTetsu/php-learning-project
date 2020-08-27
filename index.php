1ab902ff8cdd5fc41ab626f75c0e92473197aa01
MarkoTetsu <marko.fero@gmail.com>
2020-08-27 23:15:07 +0300

    simple

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+header('Cache-control: private, max-age=0');
 
-echo date('d-m-Y');
-echo "\n";
+
+if ($_SERVER['REQUEST_URI'] === '/') {
+    echo '<a href="/welcome">welcome</a>';
+    echo '<br>';
+    echo '<a href="/not-found">not-found</a>';
+} elseif ($_SERVER['REQUEST_URI'] === '/welcome') {
+    echo '<a href="/">main</a>';
+} else {
+    http_response_code(404);
+    echo 'Page not found. <a href="/">main</a>';
+}
